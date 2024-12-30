9955cf02d2b244d108b44802a49d7b165ebd924f
Fonij80 <forooz.iraji@gmail.com>
2024-12-30 22:55:38 +0330

    modify execute_tasks

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
+# Foroozan Iraji - 99105272
+# Fatemeh Sadat Lajevardi - 400105217
 from models import Task, Server, BaseStation, CriticalityLevel
 import random
 from matplotlib import pyplot as plt
-import pandas as pd
-
-
 
 arrival_time_counter = 0
-
-def initialize_base_station(number_of_servers):
-    station = BaseStation()
-    # generate servers
-    for _ in range(number_of_servers):
-        frequency = random.uniform(1.0, 3.5)  # GHz
-        transmission_rate = random.uniform(100.0, 1000.0)  # Mbps
-        cores = random.randint(4, 16)
-        station.add_server(Server(frequency, transmission_rate, cores))
-
-    return station
 
 
 def generate_tasks(number_of_tasks, criticality_level=None):
     tasks = []
     if criticality_level is None:  # if number of tasks is not based on their criticality
         Task.reset_static_fields()
+
     for i in range(number_of_tasks):
-        clocks = random.randint(1, 10) * 10
-        data_amount = random.uniform(10.0, 500.0)  # MB
+        number_of_clocks = random.randint(1, 10) * 10
+        data_amount = int(random.uniform(10.0, 500.0))  # MB
         deadline = random.randint(i, 1000)
-        criticality = criticality_level is not None if criticality_level else random.choice(list(CriticalityLevel))
-        tasks.append(Task(clocks, data_amount, deadline, criticality, i))
+        criticality = criticality_level if criticality_level is not None else random.choice(list(CriticalityLevel))
+        tasks.append(Task(number_of_clocks, data_amount, deadline, criticality, i))
 
     return tasks
 
@@ -41,68 +30,118 @@ def generate_tasks_based_on_criticality(number_of_tasks_based_on_criticality):
     return tasks
 
 
+def initialize_base_station(number_of_servers):
+    station = BaseStation()
+
+    # generate servers
+    for i in range(number_of_servers):
+        processing_frequency = int(random.uniform(1.0, 3.5))  # GHz
+        data_transmission_rate = int(random.uniform(100.0, 1000.0))  # Mbps
+        number_of_cores = random.randint(1, 5)
+        station.add_server(Server(processing_frequency, data_transmission_rate, number_of_cores))
+
+    for s in station.servers:
+        print(f"server: {s.id}, freq: {s.processing_frequency}, trans rate: {s.data_transmission_rate}, "
+              f"cors: {s.number_of_cores}")
+    return station
+
+
 def simulate(number_of_servers, tasks, isCriticalityLevelConsidered=False):
     base_station = initialize_base_station(number_of_servers)
-    base_station.create_schedule_table(tasks, isCriticalityLevelConsidered)  # assign tasks based on specified criteria and create offline table
+    # assign tasks based on specified criteria and create offline table
+    base_station.create_schedule_table(tasks, isCriticalityLevelConsidered)
 
     for server in base_station.servers:  # run each task to get its response time and is_missed
-        server.executeTasks()
+        server.execute_tasks(isCriticalityLevelConsidered)
 
-    return tasks
+
+def plot_response_time_by_criticality_level(label):
+    avg_response_times = [
+        Task.task_count_based_on_criticality[level].total_response_time /
+        Task.task_count_based_on_criticality[level].task_count
+        for level in CriticalityLevel
+    ]
+
+    plt.figure(figsize=(10, 6))
+    plt.bar([level.name for level in CriticalityLevel], avg_response_times,
+            color=['red', 'orange', 'green', 'blue'])
+    plt.title(f'Average Response Time by Criticality Level, {label}')
+    plt.xlabel('Criticality Level')
+    plt.ylabel('Average Response Time')
+    plt.xticks(rotation=45)
+    plt.grid(axis='y')
+    plt.show()
+
+
+def plot_deadline_miss_ratio_by_criticality_level(label):
+    miss_ratio = [
+        Task.task_count_based_on_criticality[level].missed_count /
+        Task.task_count_based_on_criticality[level].task_count
+        for level in CriticalityLevel
+    ]
+
+    plt.figure(figsize=(10, 6))
+    plt.bar([level.name for level in CriticalityLevel], miss_ratio,
+            color=['red', 'orange', 'green', 'blue'])
+    plt.title(f'Deadline Miss Ratio by Criticality Level, {label}')
+    plt.xlabel('Criticality Level')
+    plt.ylabel('Number of missed tasks / Number of whole tasks')
+    plt.xticks(rotation=45)
+    plt.grid(axis='y')
+    plt.show()
 
 
 if __name__ == "__main__":
-    number_of_servers = 2
+    number_of_servers = 4
 
-    # Phase 1 Outputs
-    # 40, 80, 160, 320, 640 tasks
+    # Phase 1 Outputs: Ignoring Criticality Level
     for number in [40, 80, 160, 320, 640]:
-        simulate(number_of_servers, generate_tasks(number))  # run simulation
-        # Average Response Time By Criticality
-        plt.figure(figsize=(10, 6))
-        plt.bar([CriticalityLevel.S0.level, CriticalityLevel.S1.level,
-                 CriticalityLevel.S2.level, CriticalityLevel.S3.level],
-                [Task.task_count_based_on_criticality[CriticalityLevel.S0].total_response_time,
-                 Task.task_count_based_on_criticality[CriticalityLevel.S1].total_response_time,
-                 Task.task_count_based_on_criticality[CriticalityLevel.S2].total_response_time,
-                 Task.task_count_based_on_criticality[CriticalityLevel.S3].total_response_time,],
-                color=['red', 'orange', 'green'])
-        plt.title('Average Response Time by Criticality Level')
-        plt.xlabel('Criticality Level')
-        plt.ylabel('Average Response Time (seconds)')
-        plt.xticks(rotation=45)
-        plt.grid(axis='y')
-        plt.show()
-
-        # Deadline Miss Ratio By Criticality
-        # missed_tasks = sum(task.is_missed for task in result_tasks)
+        tasks = generate_tasks(number)
+        simulate(number_of_servers, tasks)
+        plot_response_time_by_criticality_level(label=f"for {number} tasks, Ignored Criticality")
+        plot_deadline_miss_ratio_by_criticality_level(label=f"for {number} tasks, Ignored Criticality")
 
     # 320 tasks: S0: 80, S1: 80, S2: 80, S3: 80
-    number_of_tasks_based_on_criticality = {CriticalityLevel.S0: 80, CriticalityLevel.S1: 80, CriticalityLevel.S2: 80,
+    number_of_tasks_based_on_criticality = {CriticalityLevel.S0: 80, CriticalityLevel.S1: 80,
+                                            CriticalityLevel.S2: 80,
                                             CriticalityLevel.S3: 80}
-    # simulate(number_of_servers, generate_tasks_based_on_criticality(number_of_tasks_based_on_criticality))
-    # Average Response Time By Criticality
-    # Deadline Miss Ratio By Criticality
+    generate_tasks_based_on_criticality(number_of_tasks_based_on_criticality)
+    simulate(number_of_servers, tasks, isCriticalityLevelConsidered=True)
+    plot_response_time_by_criticality_level(
+        label="for 320 tasks: S0: 80, S1: 80, S2: 80, S3: 80 tasks, Considered Criticality")
+    plot_deadline_miss_ratio_by_criticality_level(
+        label="for 320 tasks: S0: 80, S1: 80, S2: 80, S3: 80 tasks, Considered Criticality")
+
+    # TODO
     # Deadline Miss Ratio By Criticality with Ltotal / 40
     # Deadline Miss Ratio By Criticality with Ltotal / 20
     # Deadline Miss Ratio By Criticality with Ltotal / 5
 
     # 320 tasks: S0: 100, S1: 80, S2: 80, S3: 60
-    number_of_tasks_based_on_criticality = {CriticalityLevel.S0: 100, CriticalityLevel.S1: 80, CriticalityLevel.S2: 80,
+    number_of_tasks_based_on_criticality = {CriticalityLevel.S0: 100, CriticalityLevel.S1: 80,
+                                            CriticalityLevel.S2: 80,
                                             CriticalityLevel.S3: 60}
-    # simulate(number_of_servers, generate_tasks_based_on_criticality(number_of_tasks_based_on_criticality))
-    # Average Response Time By Criticality
-    # Deadline Miss Ratio By Criticality
+    generate_tasks_based_on_criticality(number_of_tasks_based_on_criticality)
+    simulate(number_of_servers, tasks, isCriticalityLevelConsidered=True)
+    plot_response_time_by_criticality_level(
+        label="for 320 tasks: S0: 100, S1: 80, S2: 80, S3: 60 tasks, Considered Criticality")
+    plot_deadline_miss_ratio_by_criticality_level(
+        label="for 320 tasks: S0: 100, S1: 80, S2: 80, S3: 60 tasks, Considered Criticality")
 
     # 320 tasks: S0: 150, S1: 80, S2: 80, S3: 10
-    number_of_tasks_based_on_criticality = {CriticalityLevel.S0: 150, CriticalityLevel.S1: 80, CriticalityLevel.S2: 80,
+    number_of_tasks_based_on_criticality = {CriticalityLevel.S0: 150, CriticalityLevel.S1: 80,
+                                            CriticalityLevel.S2: 80,
                                             CriticalityLevel.S3: 10}
-    # simulate(number_of_servers, generate_tasks_based_on_criticality(number_of_tasks_based_on_criticality))
-    # Average Response Time By Criticality
-    # Deadline Miss Ratio By Criticality
+    generate_tasks_based_on_criticality(number_of_tasks_based_on_criticality)
+    simulate(number_of_servers, tasks, isCriticalityLevelConsidered=True)
+    plot_response_time_by_criticality_level(
+        label="for 320 tasks: S0: 150, S1: 80, S2: 80, S3: 10 tasks, Considered Criticality")
+    plot_deadline_miss_ratio_by_criticality_level(
+        label="for 320 tasks: S0: 150, S1: 80, S2: 80, S3: 10 tasks, Considered Criticality")
 
-    # TODO plot below outputs
-    # Phase 2 Outputs
-    # Average Response Time By Criticality with 40, 80, 160, 320, 640 tasks
-
-    # Deadline Miss Ratio By Criticality with 40, 80, 160, 320, 640 tasks
+    # Phase 2 Outputs: Considering Criticality Level
+    for number in [40, 80, 160, 320, 640]:
+        tasks = generate_tasks(number)
+        simulate(number_of_servers, tasks, isCriticalityLevelConsidered=True)
+        plot_response_time_by_criticality_level(label=f"for {number} tasks, Considered Criticality")
+        plot_deadline_miss_ratio_by_criticality_level(label=f"for {number} tasks, Considered Criticality")
--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+# Foroozan Iraji - 99105272
+# Fatemeh Sadat Lajevardi - 400105217
 from enum import Enum
 
 
@@ -44,13 +46,16 @@ class CriticalityStats:
 
 
 class Task:
+    _counter = 0
     task_count_based_on_criticality = {CriticalityLevel.S0: CriticalityStats(),
                                        CriticalityLevel.S1: CriticalityStats(),
                                        CriticalityLevel.S2: CriticalityStats(),
                                        CriticalityLevel.S3: CriticalityStats()}
 
-    def __init__(self, number_of_clocks: int, data_amount: float, deadline: int, criticality: CriticalityLevel,
+    def __init__(self, number_of_clocks: int, data_amount: int, deadline: int, criticality: CriticalityLevel,
                  arrival_time: int):
+        self.id = Task._counter
+        Task._counter += 1
         self.number_of_clocks = number_of_clocks  # ci
         self.data_amount = data_amount  # di
         self.deadline = deadline  # Ti
@@ -63,34 +68,39 @@ class Task:
         self.server = None
         Task.task_count_based_on_criticality[criticality].count_number_of_task()
 
+    def __str__(self):
+        return (f"Task {self.id}, Productivity: {self.productivity}, Assigned Server: {self.server.id}, "
+                f"Response time: {self.response_time}, Is missed: {self.is_missed}")
+
     @staticmethod
     def reset_static_fields():
+        Task._counter = 0
         Task.task_count_based_on_criticality[CriticalityLevel.S0] = CriticalityStats()
         Task.task_count_based_on_criticality[CriticalityLevel.S1] = CriticalityStats()
         Task.task_count_based_on_criticality[CriticalityLevel.S2] = CriticalityStats()
         Task.task_count_based_on_criticality[CriticalityLevel.S3] = CriticalityStats()
 
-    def execute(self):
-        pass
-
-    def suspend(self):
-        pass
-
     def get_execution_time(self, server):
-        return self.number_of_clocks / server.processing_frequency
+        return int(self.number_of_clocks / server.processing_frequency)
 
     def get_sending_delay_to_server(self, server):
-        return self.data_amount / server.data_transmission_rate
+        return int(self.data_amount / server.data_transmission_rate)
 
     def get_productivity(self, server):
-        return self.get_execution_time(server) / self.deadline
+        return round(self.get_execution_time(server) / self.deadline, 2) * 10
+
+    def update_after_assign(self, server):
+        self.arrival_time += self.get_sending_delay_to_server(server)
+        self.execution_time = self.number_of_clocks / server.processing_frequency
+        self.server = server
+        self.productivity = self.get_productivity(server)
 
 
 class Server:
     _counter = 0
 
-    def __init__(self, processing_frequency: float, data_transmission_rate: float, number_of_cores: int,
-                 productivity: float = 0):
+    def __init__(self, processing_frequency: int, data_transmission_rate: int, number_of_cores: int,
+                 productivity: int = 0):
         self.id = Server._counter
         Server._counter += 1
         self.processing_frequency = processing_frequency  # fi
@@ -100,6 +110,7 @@ class Server:
         self.number_of_assigned_tasks = {CriticalityLevel.S0: 0, CriticalityLevel.S1: 0, CriticalityLevel.S2: 0,
                                          CriticalityLevel.S3: 0}
         self.assigned_tasks = []
+        self.number_of_more_criticality_level = 0
 
     def __str__(self):
         return f"Server {self.id}, Productivity: {self.productivity}"
@@ -108,68 +119,75 @@ class Server:
         self.assigned_tasks.append(task)  # task assigned successfully
         self.update_productivity()
         self.number_of_assigned_tasks[task.criticality] += 1
-        task.arrival_time += task.get_sending_delay_to_server(self)  # update task arrival_time based on transmission's delay between base station and server
-        task.execution_time = task.number_of_clocks / self.processing_frequency
+        # update task arrival_time based on transmission's delay between base station and server
+        task.update_after_assign(self)
 
     def update_productivity(self):
-        self.productivity = sum(t.productivity for t in self.assigned_tasks)
+        self.productivity = int(sum(t.get_productivity(self) for t in self.assigned_tasks))
 
-    def executeTasks(self, isCriticalityConsidered=False):
-        # execute tasks based on FIFO
+    def execute_tasks(self, is_criticality_considered=False):
+        # Sort assigned tasks based on arrival time
         sorted_tasks_based_on_arrival_time = sorted(self.assigned_tasks, key=lambda t: t.arrival_time)
-        print(f"cccccc: {len(sorted_tasks_based_on_arrival_time)}")
-        current_time = 0
+        current_time = sorted_tasks_based_on_arrival_time[0].arrival_time
         arrived_tasks = []
-        while len(sorted_tasks_based_on_arrival_time) != 0:
-            # print(f"current time {current_time}")
-            for task in sorted_tasks_based_on_arrival_time:
-                print(f"arrive at: {task.arrival_time}")
-                if task.arrival_time == current_time:
-                    arrived_tasks.append(task)
 
+        while sorted_tasks_based_on_arrival_time or arrived_tasks:
+            # Check for new arrivals at the current time
+            while sorted_tasks_based_on_arrival_time and sorted_tasks_based_on_arrival_time[
+                0].arrival_time == current_time:
+                arrived_tasks.append(sorted_tasks_based_on_arrival_time.pop(0))
+
+            if not arrived_tasks:
+                current_time = sorted_tasks_based_on_arrival_time[0].arrival_time
+                continue  # Check for new arrivals at arrival time of first task in list
+
+            # Process each arrived task in FIFO order
             for task in arrived_tasks:
-                if not isCriticalityConsidered:
+                if not is_criticality_considered:
                     # Check if the deadline will be missed
                     if current_time + task.execution_time > task.deadline:
                         task.is_missed = True
                         Task.task_count_based_on_criticality[task.criticality].count_missed_task()
-                        arrived_tasks.remove(task)
-                        sorted_tasks_based_on_arrival_time.remove(task)
-                        continue  # don't execute the task and go to next arrived task
-
-                    current_time += task.execution_time  # finish time
+                        arrived_tasks.remove(task)  # Remove missed task from arrived tasks
+                        continue
+                    # Execute the task
+                    current_time += task.execution_time  # Update current time after execution
                     task.response_time = current_time - task.arrival_time
                     Task.task_count_based_on_criticality[task.criticality].sum_response_time(task.response_time)
-                    arrived_tasks.remove(task)  # task executed successfully
-                    sorted_tasks_based_on_arrival_time.remove(task)
-                    print(f'{current_time} , {task.execution_time}')
-                    # Update server's productivity after executing the task
-                    self.update_productivity()
+                    arrived_tasks.remove(task)  # Task executed successfully
+                    self.update_productivity()  # Update server's productivity after executing the task
+                    print(f'Task {task.id} arrived at {task.arrival_time}, finished at: {current_time}')
 
-                else:  # execute based on criticality level
+                else:  # Execute based on criticality level
                     sorted_arrived_tasks_based_on_criticality = sorted(arrived_tasks, key=lambda t: t.criticality.level)
-                    # TODO complete execution based on criticality level
+                    # TODO
+                    return
 
 
 class BaseStation:
     def __init__(self):
+        Server._counter = 0
         self.servers = []
 
     def add_server(self, server):
         self.servers.append(server)
 
     def create_schedule_table(self, tasks, isCriticalityLevelConsidered):
-        # assign tasks based on server productivity
-        sorted_servers_based_on_productivity = sorted(self.servers, key=lambda s: s.productivity)
-
         for i in range(len(tasks)):
+            # assign tasks based on server productivity
+            sorted_servers_based_on_productivity = sorted(self.servers, key=lambda s: s.productivity)
+            print(f"Task: {tasks[i].id}, clocks: {tasks[i].number_of_clocks}, arrival: {tasks[i].arrival_time}")
             for server in sorted_servers_based_on_productivity:
+                print(f"Server: {server.id}, productivity: {server.productivity}")
                 if server.productivity + tasks[i].get_productivity(server) <= server.number_of_cores:
                     server.assign_task(tasks[i])
+                    print(
+                        f"Task {tasks[i].id} assigned to Server {server.id}, task productivity: {tasks[i].get_productivity(server)}, cors: {server.number_of_cores} First if")
                     break  # go to next task
                 elif not isCriticalityLevelConsidered:  # no server can meet the task's deadline
-                    sorted_servers_based_on_productivity[0].assign_task(
-                        tasks[i])  # assign task to server with the least productivity
+                    # assign task to server with the least productivity
+                    print(f"Task {tasks[i].id} assigned to Server {server.id}, Second if")
+                    sorted_servers_based_on_productivity[0].assign_task(tasks[i])
                     break  # go to next task
                 else:  # schedule based on criticality level
                     server.number_of_more_criticality_level = 0
